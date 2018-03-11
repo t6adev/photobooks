fb26d0b7c9a40cf424aaeffc6ca36c6c018a7831
tell-y <t.yamamoto28@gmail.com>
2018-03-11 22:24:08 +0900

    WIP:add image picker from example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ImagePicker from './ImagePicker';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>app!</Text>
+        <ImagePicker />
       </View>
     );
   }
