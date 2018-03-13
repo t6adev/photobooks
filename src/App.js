9d341995ea674933fd89edd89a18e931c82443b2
tell-y <t.yamamoto28@gmail.com>
2018-03-13 23:47:28 +0900

    insert picked img to array

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import ImagePicker from './ImagePicker';
 export default class App extends React.Component {
   state = {
     pickedImageInfo: null,
+    images: [],
   };
   render() {
     return (
       <View style={styles.container}>
         <Text>app!</Text>
-        <ImagePicker pickedUp={info => this.setState({ pickedImageInfo: info })}/>
-        {this.state.pickedImageInfo && <Image source={{ uri: this.state.pickedImageInfo.uri }} style={{ width: 200, height: 200 }} />}
+        <ImagePicker pickedUp={info => this.setState({ pickedImageInfo: info, images: [info, ...this.state.images] })}/>
+        {this.state.images.map(img => <Image key={img.uri} source={{ uri: img.uri }} style={{ width: 200, height: 200 }} />)}
       </View>
     );
   }
