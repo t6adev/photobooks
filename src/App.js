0f20e3c686285c5ee646dbdc87e72ee5072ad7cc
tell-y <t.yamamoto28@gmail.com>
2018-03-13 23:52:12 +0900

    add ScrollView for images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, Image, View } from 'react-native';
+import { StyleSheet, Text, Image, View, ScrollView } from 'react-native';
 import ImagePicker from './ImagePicker';
 
 export default class App extends React.Component {
@@ -11,8 +11,10 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>app!</Text>
-        <ImagePicker pickedUp={info => this.setState({ pickedImageInfo: info, images: [info, ...this.state.images] })}/>
-        {this.state.images.map(img => <Image key={img.uri} source={{ uri: img.uri }} style={{ width: 200, height: 200 }} />)}
+        <ScrollView>
+          <ImagePicker pickedUp={info => this.setState({ pickedImageInfo: info, images: [info, ...this.state.images] })}/>
+          {this.state.images.map(img => <Image key={img.uri} source={{ uri: img.uri }} style={{ width: 200, height: 200 }} />)}
+        </ScrollView>
       </View>
     );
   }
