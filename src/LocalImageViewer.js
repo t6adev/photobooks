1521d1719f281a52e48d6c699b5b505cfc4747a6
tell-y <t.yamamoto28@gmail.com>
2018-03-17 00:20:46 +0900

    WIP:x columns view

--- src/LocalImageViewer.js
+++ src/LocalImageViewer.js
@@ -1,27 +1,53 @@
 import React from 'react';
-import { View, ScrollView, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, ScrollView, Image, TouchableOpacity, FlatList } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+const ImageBox = ({ node }) => (
+  <TouchableOpacity key={node.image.uri} onPress={() => this.onPress(node)}>
+    <Image
+      style={{
+        width: 100,
+        height: 100,
+      }}
+      source={{ uri: node.image.uri }}
+    />
+  </TouchableOpacity>
+);
+
+const buildColums = colNum => (cols, item, index) => {
+  cols[Math.ceil((index + 1) / colNum) - 1].push(item);
+  return cols;
+};
 
 export default class LocalImageViewerComponent extends React.Component {
-  onPress = (node) => {
+  onPress = node => {
     console.log(node);
-  }
+  };
   render() {
+    const { images, colNum = 2 } = this.props;
+    const rowNum = Math.ceil(images.length / colNum);
+    const colArry = Array.from({ length: rowNum }, (v, k) => k).map(() => []);
+    console.log({ rowNum, colArry });
+    const res = images.map(({ node }) => node).reduce(buildColums(colNum), colArry);
+    console.log(res);
     return (
       <View>
         <ScrollView>
-          {this.props.images.map(({ node }) => {
-            return (
-              <TouchableOpacity key={node.image.uri} onPress={() => this.onPress(node)}>
-                <Image
-                  style={{
-                    width: 100,
-                    height: 100,
-                  }}
-                  source={{ uri: node.image.uri }}
-                />
-              </TouchableOpacity>
-            );
-          })}
+          {images
+            .map(({ node }) => node)
+            .reduce(buildColums(colNum), colArry)
+            .map(cols => (
+              <View key={cols.length} style={styles.container}>
+                {cols.map(node => <ImageBox key={node.image.uri} node={node} />)}
+              </View>
+            ))}
         </ScrollView>
       </View>
     );
