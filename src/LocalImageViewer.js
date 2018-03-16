f7b0bf592fb6b9815461373c254d711356382a0c
tell-y <t.yamamoto28@gmail.com>
2018-03-16 20:55:20 +0900

    wrap an Image in TouchableOpacity

--- src/LocalImageViewer.js
+++ src/LocalImageViewer.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import { View, ScrollView, Image } from 'react-native';
+import { View, ScrollView, Image, TouchableOpacity } from 'react-native';
 
 export default class LocalImageViewerComponent extends React.Component {
+  onPress = (node) => {
+    console.log(node);
+  }
   render() {
     return (
       <View>
         <ScrollView>
           {this.props.images.map(({ node }) => {
             return (
-              <Image
-                key={node.image.uri}
-                style={{
-                  width: 100,
-                  height: 100,
-                }}
-                source={{ uri: node.image.uri }}
-              />
+              <TouchableOpacity key={node.image.uri} onPress={() => this.onPress(node)}>
+                <Image
+                  style={{
+                    width: 100,
+                    height: 100,
+                  }}
+                  source={{ uri: node.image.uri }}
+                />
+              </TouchableOpacity>
             );
           })}
         </ScrollView>
