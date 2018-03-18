3773f6ea1024e9cf08173efe94d8d0f755ee7408
tell-y <t.yamamoto28@gmail.com>
2018-03-18 22:23:50 +0900

    add tab navigator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, Image, View, ScrollView } from 'react-native';
+import { TabNavigator } from 'react-navigation';
 import LocalImagePicker from './LocalImagePicker';
 import LocalImageViewer from './LocalImageViewer';
-
-export default class App extends React.Component {
-  state = {
-    imageRoll: { images: [], pageInfo: null },
-  };
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>app!</Text>
-        <ScrollView>
-          <LocalImagePicker onClick={info => this.setState({ imageRoll: { ...info } })}/>
-          <LocalImageViewer images={this.state.imageRoll.images} />
-        </ScrollView>
-      </View>
-    );
-  }
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -27,4 +11,31 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+});
+
+const HomeScreen = () => (
+  <View style={styles.container}>
+    <Text>Home!</Text>
+  </View>
+);
+
+class ImagesScreen extends React.Component {
+  state = {
+    imageRoll: { images: [], pageInfo: null },
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <LocalImagePicker onClick={info => this.setState({ imageRoll: { ...info } })}/>
+        <ScrollView>
+          <LocalImageViewer images={this.state.imageRoll.images} />
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
+export default TabNavigator({
+  Home: { screen: HomeScreen },
+  Images: { screen: ImagesScreen },
 });
