79e74d8c5925a89360ca7d1501d1b35dee2dd607
tell-y <t.yamamoto28@gmail.com>
2018-04-03 21:57:29 +0900

    WIP:for album area

---
+++ src/Album.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const commentBtnTitle = ['Edit', 'Complete'];
+
+export default class AlbumComponent extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>One Album</Text>
+      </View>
+    );
+  }
+}
---
+++ src/Albums.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const commentBtnTitle = ['Edit', 'Complete'];
+
+export default class AlbumsComponent extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Album list</Text>
+      </View>
+    );
+  }
+}
