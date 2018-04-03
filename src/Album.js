e39c058986f31aa7e1781271830e5edc28bf7bc5
tell-y <t.yamamoto28@gmail.com>
2018-04-03 21:57:46 +0900

    fix

--- src/Album.js
+++ src/Album.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const commentBtnTitle = ['Edit', 'Complete'];
-
 export default class AlbumComponent extends React.Component {
   render() {
     return (
--- src/Albums.js
+++ src/Albums.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const commentBtnTitle = ['Edit', 'Complete'];
-
 export default class AlbumsComponent extends React.Component {
   render() {
     return (
