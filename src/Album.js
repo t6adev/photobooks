33bd86c187b6750b47097fd943f043bcc5974147
tell-y <t.yamamoto28@gmail.com>
2018-04-04 20:25:39 +0900

    WIP:add features

--- src/Album.js
+++ src/Album.js
@@ -3,9 +3,14 @@ import { View, Text } from 'react-native';
 
 export default class AlbumComponent extends React.Component {
   render() {
+    const { info } = this.props;
+    const { title, tags, description, photos } = info;
     return (
       <View>
-        <Text>One Album</Text>
+        <Text>{title}</Text>
+        <Text>{tags.map(tag => `#${tag}`).join(' ')}</Text>
+        <Text>{description}</Text>
+        {/* <View>{photos}</View> */}
       </View>
     );
   }
--- src/Albums.js
+++ src/Albums.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import Album from './Album';
 
 export default class AlbumsComponent extends React.Component {
   render() {
+    const { albums } = this.props;
     return (
       <View>
-        <Text>Album list</Text>
+        {albums.map(album => <Album info={album} />)}
       </View>
     );
   }
