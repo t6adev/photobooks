5dd2ee8ac942e1cc2e2278cd225976a0f2806209
tell-y <t.yamamoto28@gmail.com>
2018-04-09 23:41:43 +0900

    WIP:add editing place

--- src/Album.js
+++ src/Album.js
@@ -1,21 +1,47 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, TextInput } from 'react-native';
 import PhotoPage from './PhotoPage';
 
 export default class AlbumComponent extends React.Component {
   render() {
-    const { info } = this.props;
+    const { info, editing } = this.props;
     const { title, tags, description, photos } = info;
     return (
       <View>
-        <Text>{title}</Text>
-        <Text>{tags.map(tag => `#${tag}`).join(' ')}</Text>
-        <Text>{description}</Text>
-        <View>{photos.map(photo => (
-          <View>
-            <Image source={{ uri: photo.uri }} />
-          </View>
-        ))}</View>
+        {editing ? (
+          <TextInput
+            onChangeText={() => {}}
+            // value={}
+          />
+        ) : (
+          <Text>{title}</Text>
+        )}
+        {editing ? (
+          <TextInput
+            onChangeText={() => {}}
+            // value={}
+          />
+        ) : (
+          <Text>{tags.map(tag => `#${tag}`).join(' ')}</Text>
+        )}
+        {editing ? (
+          <TextInput
+            multiline={true}
+            numberOfLines={4}
+            onChangeText={() => {}}
+            // value={}
+            editable={true}
+          />
+        ) : (
+          <Text>{description}</Text>
+        )}
+        <View>
+          {photos.map(photo => (
+            <View>
+              <Image source={{ uri: photo.uri }} />
+            </View>
+          ))}
+        </View>
       </View>
     );
   }
